4950354e068a7ddfb3200e6f267b5be4ff40ebc0
Daniel Williams <danielwil621@gmail.com>
2021-04-03 17:37:22 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,15 +1,16 @@
-// function forLoop(array){
-//   for(let i = 0; i < 25; i++){
-//     if(i === 1){
-//       console.log("I am 1 strange loop");
-//     }else{
-//       console.log(`I am ${i} strange loops`);
-//     }
-//   }
-// }
-
 function forLoop(array){
   for(let i = 0; i < 25; i++){
-    (i < 25) ? array.push("I am 1 strange loop.") : array.push(`I am ${i} strange loops.`);
+    if(i === 1){
+      console.log("I am 1 strange loop");
+    }else{
+      console.log(`I am ${i} strange loops`);
+    }
   }
+  return array;
 }
+
+// function forLoop(array){
+//   for(let i = 0; i < 25; i++){
+//     (i < 25) ? array.push("I am 1 strange loop.") : array.push(`I am ${i} strange loops.`);
+//   }
+// }
