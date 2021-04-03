b5502c3abc0403c8c0ad3a3f5326a1a0e4f73bcb
Daniel Williams <danielwil621@gmail.com>
2021-04-03 17:41:11 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@
 //     if(i === 1){
 //      array.push("I am 1 strange loop.");
 //     }else{
-//       console.log(`I am ${i} strange loops`);
+//       array.push(`I am ${i} strange loops.`);
 //     }
 //   }
 //   return array;
